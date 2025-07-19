7785d97454ba189783b51ee4f6a129642771f966
Mikail Pirgozi <mikailpirgozi@MacBook-Pro-uzivatela-Mikail-2.local>
2025-07-19 20:18:13 +0200

    🔧 FIX: Add dayjs plugins setup to fix DateTimePicker isBefore error

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import './utils/dayjs-setup';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
