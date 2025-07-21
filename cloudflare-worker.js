29d8cda066047f1c8768c8fcab585de44619e918
Mikail Pirgozi <mikail@blackrent.sk>
2025-07-21 22:44:38 +0200

    Opravený R2 SSL problém - Worker používa backend proxy URL

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -79,8 +79,8 @@ export default {
         },
       });
 
-      // Vytvorenie public URL
-      const publicUrl = `${env.R2_PUBLIC_URL}/${fileKey}`;
+      // Vytvorenie public URL (používame backend proxy kvôli R2 SSL problému)
+      const publicUrl = `https://blackrent-app-production-4d6f.up.railway.app/api/files/proxy/${encodeURIComponent(fileKey)}`;
 
       // Vytvorenie photo objektu
       const photoObject = {
